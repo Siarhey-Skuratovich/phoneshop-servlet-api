e3e705aedf587daaa0f953014894e0e2b22d5f51
Siarhey-Skuratovich <siarheyskuratovich@gmail.com>
2022-01-22 13:59:08 +0300

    Task 1.5 Implemented ProductDao

--- src/main/java/com/es/phoneshop/model/product/Product.java
+++ src/main/java/com/es/phoneshop/model/product/Product.java
@@ -4,82 +4,95 @@ import java.math.BigDecimal;
 import java.util.Currency;
 
 public class Product {
-    private Long id;
-    private String code;
-    private String description;
-    /** null means there is no price because the product is outdated or new */
-    private BigDecimal price;
-    /** can be null if the price is null */
-    private Currency currency;
-    private int stock;
-    private String imageUrl;
+  private Long id;
+  private String code;
+  private String description;
+  /**
+   * null means there is no price because the product is outdated or new
+   */
+  private BigDecimal price;
+  /**
+   * can be null if the price is null
+   */
+  private Currency currency;
+  private int stock;
+  private String imageUrl;
 
-    public Product() {
-    }
+  public Product() {
+  }
 
-    public Product(Long id, String code, String description, BigDecimal price, Currency currency, int stock, String imageUrl) {
-        this.id = id;
-        this.code = code;
-        this.description = description;
-        this.price = price;
-        this.currency = currency;
-        this.stock = stock;
-        this.imageUrl = imageUrl;
-    }
+  public Product(Long id, String code, String description, BigDecimal price, Currency currency, int stock, String imageUrl) {
+    this.id = id;
+    this.code = code;
+    this.description = description;
+    this.price = price;
+    this.currency = currency;
+    this.stock = stock;
+    this.imageUrl = imageUrl;
+  }
 
-    public Long getId() {
-        return id;
-    }
+  public Product(String code, String description, BigDecimal price, Currency currency, int stock, String imageUrl) {
+    this.code = code;
+    this.description = description;
+    this.price = price;
+    this.currency = currency;
+    this.stock = stock;
+    this.imageUrl = imageUrl;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public String getCode() {
-        return code;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
+  public String getCode() {
+    return code;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public void setCode(String code) {
+    this.code = code;
+  }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+  public String getDescription() {
+    return description;
+  }
 
-    public BigDecimal getPrice() {
-        return price;
-    }
+  public void setDescription(String description) {
+    this.description = description;
+  }
 
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
+  public BigDecimal getPrice() {
+    return price;
+  }
 
-    public Currency getCurrency() {
-        return currency;
-    }
+  public void setPrice(BigDecimal price) {
+    this.price = price;
+  }
 
-    public void setCurrency(Currency currency) {
-        this.currency = currency;
-    }
+  public Currency getCurrency() {
+    return currency;
+  }
 
-    public int getStock() {
-        return stock;
-    }
+  public void setCurrency(Currency currency) {
+    this.currency = currency;
+  }
 
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
+  public int getStock() {
+    return stock;
+  }
 
-    public String getImageUrl() {
-        return imageUrl;
-    }
+  public void setStock(int stock) {
+    this.stock = stock;
+  }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
 }
